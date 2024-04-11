c883f94b7cd2c46cb2cb3e0ba149ceadfb1aec45
Yusra Yasir <https://github.com/YusraYasir/simple_calculator.git>
2024-04-11 15:14:48 -0700

    Currency_Converter

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Project_Currency_Converter_App" 
---
+++ index.ts
@@ -0,0 +1,55 @@
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.rgb(204,204,204)(`\t\t<<< ========================================================= >>> `));
+console.log( chalk.blueBright.bold("\n\t\t        Welcome to codewithYusra - Currency_Convertor_App\n"));
+console.log(chalk.bold.rgb(204,204,204)(`\t\t<<< ========================================================= >>> `));
+
+let apiLink = "https://v6.exchangerate-api.com/v6/56d86d580375615c7b753480/latest/pkr";
+
+let fetchData = async (data: any) => {
+    let fetchData = await fetch(data);
+    let res = await fetchData.json();
+    return res.conversion_rates;
+}
+
+let data = await fetchData(apiLink);
+
+let countries = Object.keys(data);
+
+let firstCountry = await inquirer.prompt({
+    type: "list",
+    name: "name",
+    message: "Converting from",
+    choices: countries
+});
+
+let userMoney = await inquirer.prompt({
+    type: "number",
+    name: "rupee",
+    message: `Please enter amount in ${chalk.greenBright.bold(firstCountry.name)}`
+});
+
+let secondCountry = await inquirer.prompt({
+    type: "list",
+    name: "name",
+    message: "Converting to",
+    choices: countries
+});
+
+let conversionData = `https://v6.exchangerate-api.com/v6/3fd78a6341a168ddf6863092/pair/${firstCountry.name}/${secondCountry.name}`;
+
+let conversion = async (data: any) => {
+    let conversion = await fetch(data);
+    let res = await conversion.json();
+    return res.conversion_rate;
+};
+
+let conversionRate = await conversion(conversionData);
+
+let convertedRate = userMoney.rupee * conversionRate;
+
+console.log(`Your ${chalk.greenBright.bold(firstCountry.name)} ${chalk.greenBright.bold(userMoney.rupee.toLocaleString())} 
+in ${chalk.greenBright.bold(secondCountry.name)} is ${chalk.greenBright.bold(convertedRate.toLocaleString())}`);
+
